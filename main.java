1bc14e13ccab233026299b1c4d704ffb74474069
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-20 11:39:46 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+//file name should be Named as Switch.java
+package Switch;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		switch1 k=new switch1();
+		Information jh=new Information();
+		k.head();
+		k.swit();
+		jh.info();
+		k=null;
+		
+	}
+
+}
